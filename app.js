823ea9190186887df9d14f118e1837d8e3df1ece
danny <danixeka@gmail.com>
2019-03-17 09:03:43 +1100

    added yargs and some commands

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const ch = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(process.argv);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note.',
+  handler: function () {
+    console.log('Adding a new note!');
+  }
+})
 
-if (command === 'add') {
-  console.log('Adding note!');
-} else if (command === 'remove') {
-  console.log('removing note!');
-}
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note.',
+  handler: function () {
+    console.log('Removing the note!');
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read the note.',
+  handler: function () {
+    console.log('Here is the note...');
+  }
+})
+
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Shows a listing of the note.',
+  handler: function () {
+    console.log('Here is a list of all your notes...');
+  }
+})
+
+
+
+console.log(yargs.argv);
